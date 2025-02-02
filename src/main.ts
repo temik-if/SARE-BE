c1458199f26335a4ac3da63e6b4278bfbc5a0f5c
efraimff <emfs6@aluno.ifal.edu.br>
2025-02-01 22:37:33 -0300

    feat: configure global app settings in main.ts
    - Enabled CORS to allow cross-origin requests.
    - Added global validation pipe to automatically validate incoming requests.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
   // Configuração básica do Swagger
   const config = new DocumentBuilder()
