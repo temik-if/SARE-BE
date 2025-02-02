9cb5c6cd4bc173fa8a0945a50920e16c538faa4d
efraimff <emfs6@aluno.ifal.edu.br>
2025-02-01 22:55:10 -0300

    chore: add dist and .idea to .gitignore and remove from version control

--- dist/modules/user/dto/create-user.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserType } from "@prisma/client";
-export declare class CreateUserDto {
-    first_name: string;
-    last_name: string;
-    email: string;
-    password: string;
-    type: UserType;
-}
