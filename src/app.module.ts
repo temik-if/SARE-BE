a1c40258fa0c9bf4e4618f850137682f87f34a0e
efraimff <emfs6@aluno.ifal.edu.br>
2025-02-04 16:58:05 -0300

    chore: configure environment variables for JWT
    - Added JWT_SECRET to .env file.
    - Configured ConfigModule to load environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from "./prisma.service";
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule, ConfigModule.forRoot({})],
   providers: [PrismaService]
 })
 
