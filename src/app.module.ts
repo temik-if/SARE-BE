a9cef4983384a5d930c43283600e72970657ea01
efraimff <emfs6@aluno.ifal.edu.br>
2025-02-06 16:15:53 -0300

    feat: add ResourceModule to AppModule
    - Imported ResourceModule to the AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ResourceModule } from './modules/resource/resource.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule, ConfigModule.forRoot({})],
+  imports: [UserModule, AuthModule, PrismaModule, ResourceModule, ConfigModule.forRoot({})],
 })
 
 export class AppModule {}
