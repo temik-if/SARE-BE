b5e5e1802c447df075a63fcc126fa66c8a2d3673
tanyse <108136417+tanyse@users.noreply.github.com>
2025-02-11 06:17:41 -0300

    create module penalties

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ResourceModule } from './modules/resource/resource.module';
 import { PenaltiesController } from './modules/penalties/penalties.controller';
 import { PenaltiesService } from './penalties/penalties.service';
+import { PenaltiesModule } from './modules/penalties/penalties.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule, ResourceModule, ConfigModule.forRoot({})],
+  imports: [UserModule, AuthModule, PrismaModule, ResourceModule, ConfigModule.forRoot({}), PenaltiesModule],
   controllers: [PenaltiesController],
   providers: [PenaltiesService],
 })
