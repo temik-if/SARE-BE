520850d2e6972aacf549d43295914406c464d780
efraimff <emfs6@aluno.ifal.edu.br>
2025-03-03 10:14:59 -0300

    chore: register BookingModule in AppModule
    - Imported Booking module into app.module.ts to register the new module for booking functionality.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ResourceModule } from './modules/resource/resource.module';
+import { BookingModule } from './modules/booking/booking.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule, ResourceModule, ConfigModule.forRoot({})],
+  imports: [UserModule, AuthModule, PrismaModule, ResourceModule, BookingModule, ConfigModule.forRoot({})],
 })
 
 export class AppModule {}
