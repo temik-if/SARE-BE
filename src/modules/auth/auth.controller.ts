dbc00a4a62f8c6f8c182b27cfcf9558e00a0f08c
Misael Alisson Ursulino de Lima <maul1@aluno.ifal.edu.br>
2025-03-04 11:15:16 -0300

    feat: implement Google authentication and add GoogleDto for login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,34 +3,48 @@ import { AuthService } from "./auth.service";
 import { LoginDto } from './dtos/login.dto';
 import { ApiBody, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { GoogleDto } from './dtos/google.dto';
 
-@Controller('user/auth')
+@Controller("user/auth")
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('login')
-    @ApiOperation({ 
-        summary: 'Login',
-        description: 'Authenticates the user and returns an access token'
-    })
-    @ApiBody({ type: LoginDto })
-    @ApiResponse({ status: 200, description: 'Login successfully' })
-    @ApiResponse({ status: 400, description: 'Bad Request' })
-    @ApiResponse({ status: 401, description: 'Unauthorized' })
-    async login(@Body() data: LoginDto) {
-        return this.authService.login(data);
-    }
+  @Post("login")
+  @ApiOperation({
+    summary: "Login",
+    description: "Authenticates the user and returns an access token",
+  })
+  @ApiBody({ type: LoginDto })
+  @ApiResponse({ status: 200, description: "Login successfully" })
+  @ApiResponse({ status: 400, description: "Bad Request" })
+  @ApiResponse({ status: 401, description: "Unauthorized" })
+  async login(@Body() data: LoginDto) {
+    return this.authService.login(data);
+  }
 
-    @Get('me')
-    @UseGuards(JwtAuthGuard)
-    @ApiOperation({ 
-        summary: 'Get the authenticated user',
-        description: 'Returns the details of the authenticated user'
-    })
-    @ApiResponse({ status: 200, description: 'Return the authenticated user' })
-    @ApiResponse({ status: 401, description: 'Unauthorized' })
-    @ApiBearerAuth()
-    async me(@Request() req) {
-        return req.user;
-    }
+  @Post("google")
+  @ApiOperation({
+    summary: "Verify login with Google",
+    description: "Authenticates the user with Google and returns an access token",
+  })
+  @ApiBody({ type: GoogleDto })
+  @ApiResponse({ status: 200, description: "Login successful" })
+  @ApiResponse({ status: 400, description: "Bad Request" })
+  @ApiResponse({ status: 401, description: "Unauthorized" })
+  async verifyAndLoginWithGoogle(@Body() data: GoogleDto) {
+    return this.authService.loginWithGoogle(data);
+  }
+
+  @Get("me")
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    summary: "Get the authenticated user",
+    description: "Returns the details of the authenticated user",
+  })
+  @ApiResponse({ status: 200, description: "Return the authenticated user" })
+  @ApiResponse({ status: 401, description: "Unauthorized" })
+  @ApiBearerAuth()
+  async me(@Request() req) {
+    return req.user;
+  }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,32 +1,82 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from "bcrypt";
 import { omit } from "lodash";
-import  { JwtService } from '@nestjs/jwt';
-import { UserService } from '../user/user.service';
+import { JwtService } from "@nestjs/jwt";
+import { UserService } from "../user/user.service";
+import { OAuth2Client } from "google-auth-library";
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly userService: UserService,
-        private readonly jwtService: JwtService
-    ) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService
+  ) {}
 
-    async validateUser (email: string, password: string) {
-        const user = await this.userService.findUserWithPassword(email);
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findUserWithPassword(email);
 
-        if (user && (await bcrypt.compare(password, user.password)) && user.is_active == true) {
-            return omit(user, ['password']);
-        } else {
-            throw new UnauthorizedException('Invalid credentials');
-        }
+    if (
+      user &&
+      (await bcrypt.compare(password, user.password)) &&
+      user.is_active == true
+    ) {
+      return omit(user, ["password"]);
+    } else {
+      throw new UnauthorizedException("Invalid credentials");
+    }
+  }
+
+  async login(user: any) {
+    const validatedUser = await this.validateUser(user.email, user.password);
+    const payload = {
+      email: validatedUser.email,
+      sub: validatedUser.id,
+      type: validatedUser.type,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user: validatedUser,
+    };
+  }
+
+  async loginWithGoogle(data: any) {
+    const { email, id_token } = data;
+    const googlePayload = await this.verifyGoogleToken(id_token);
+    if (googlePayload == undefined) {
+      throw new UnauthorizedException("Invalid ID token");
+    }
+    
+    
+    const user = await this.userService.findUserWithPassword(email);
+    if (!user || user.is_active == false) {
+      throw new UnauthorizedException(
+        "Invalid credentials or unauthorized user"
+      );
     }
 
-    async login(user: any) { 
-        const validatedUser = await this.validateUser(user.email, user.password);
-        const payload = { email: validatedUser.email, sub: validatedUser.id, type: validatedUser.type };
-        return {
-            access_token: this.jwtService.sign(payload),
-            user: validatedUser
-        };
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: user.type,
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+      user: omit(user, ["password"]),
+    };
+  }
+
+  async verifyGoogleToken(idToken) {
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken,
+        audience: process.env.GOOGLE_CLIENT_ID,
+      });
+      return ticket.getPayload();
+    } catch (error) {
+      return undefined;
     }
+  }
 }
