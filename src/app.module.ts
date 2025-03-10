5ededc8ffbed4d3477df88a4ab82f6fc4ee361f5
efraimff <emfs6@aluno.ifal.edu.br>
2025-03-09 22:15:53 -0300

    chore: register penalty module in app and booking modules
    - Imported penalty module into app module to integrate it into the system.
    - Added penalty module to booking module imports to allow service injection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ResourceModule } from './modules/resource/resource.module';
 import { BookingModule } from './modules/booking/booking.module';
+import { PenaltyModule } from './modules/penalty/penalty.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule, ResourceModule, BookingModule, ConfigModule.forRoot({})],
+  imports: [UserModule, AuthModule, PrismaModule, ResourceModule, BookingModule, PenaltyModule, ConfigModule.forRoot({})],
 })
 
 export class AppModule {}
--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -3,11 +3,12 @@ import { BookingService } from "./booking.service";
 import { BookingController } from "./booking.controller";
 import { ResourceModule } from "../resource/resource.module";
 import { UserModule } from "../user/user.module";
+import { PenaltyModule } from "../penalty/penalty.module";
 
 @Module({
     controllers: [BookingController],
     providers: [BookingService],
-    imports: [ResourceModule, UserModule]
+    imports: [ResourceModule, UserModule, PenaltyModule]
 })
 
 export class BookingModule {}
