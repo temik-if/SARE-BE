18f2e17c952593bfc3d4e7dadbcd9c90690f4235
efraimff <emfs6@aluno.ifal.edu.br>
2025-03-09 22:05:42 -0300

    refactor: improve Swagger documentation in DTOs
    - Updated update-booking.dto.ts and update-booking-status.dto.ts.
    - Improved example values to enhance Swagger API documentation.

--- src/modules/booking/dtos/update-booking-status.dto.ts
+++ src/modules/booking/dtos/update-booking-status.dto.ts
@@ -3,7 +3,7 @@ import { BookingStatus } from "@prisma/client";
 import { IsEnum, IsNotEmpty } from "class-validator";
 
 export class UpdateBookingStatusDto {
-    @ApiProperty({ example: 'IN_PROGRESS', description: 'The status of the booking' })
+    @ApiProperty({ example: 'COMPLETED', description: 'The status of the booking' })
     @IsNotEmpty({ message: 'Status is required' })
     @IsEnum(['IN_PROGRESS', 'COMPLETED', 'CANCELLED'], { message: 'Invalid status, choose between IN_PROGRESS, COMPLETED, or CANCELLED' })
     status: BookingStatus;
--- src/modules/booking/dtos/update-booking.dto.ts
+++ src/modules/booking/dtos/update-booking.dto.ts
@@ -3,16 +3,16 @@ import { CreateBookingDto } from "./create-booking.dto";
 import { BookingStatus, ShiftType } from "@prisma/client";
 
 export class UpdateBookingDto extends PartialType(CreateBookingDto) {
-    @ApiProperty({ example: 1, description: 'The id of the resource' })
+    @ApiProperty({ example: 2, description: 'The id of the resource' })
     resource_id: number;
 
-    @ApiProperty({ example: 'MORNING', description: 'The shift of the booking' })
+    @ApiProperty({ example: ShiftType.AFTERNOON, description: 'The shift of the booking' })
     shift: ShiftType;
 
-    @ApiProperty({ example: '2025-03-01', description: 'The date of the booking' })
+    @ApiProperty({ example: '2025-04-01', description: 'The date of the booking' })
     date: string;
 
-    @ApiProperty({ example: [1, 2, 3], description: 'The class of the booking' })
+    @ApiProperty({ example: [1, 2], description: 'The class of the booking' })
     class: number[];
 
     @ApiProperty({ example: 'IN_PROGRESS', description: 'The status of the booking' })
