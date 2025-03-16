f491c1800d2d4064eb234eba965d5cab45aac3e4
efraimff <emfs6@aluno.ifal.edu.br>
2025-03-16 11:16:10 -0300

    chore: add EmailModule and PasswordResetModule to AppModule imports
    - Added EmailModule to manage email-related functionalities.
    - Added PasswordResetModule to handle password recovery operations.
    - Updated AppModule imports to include the new modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ResourceModule } from './modules/resource/resource.module';
 import { BookingModule } from './modules/booking/booking.module';
 import { PenaltyModule } from './modules/penalty/penalty.module';
+import { EmailModule } from './modules/email/email.module';
+import { PasswordResetModule } from './modules/password-reset/password-reset.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule, ResourceModule, BookingModule, PenaltyModule, ConfigModule.forRoot({})],
+  imports: [UserModule, AuthModule, PrismaModule, ResourceModule, BookingModule, EmailModule, PasswordResetModule, PenaltyModule, ConfigModule.forRoot({})],
 })
 
 export class AppModule {}
