f5b8a34a7bb305324c0fab483015747e7ca9a1cc
efraimff <emfs6@aluno.ifal.edu.br>
2025-03-21 18:26:16 -0300

    feat: add verification module for activation and password reset
    - Created VerificationService to handle verification codes.
    - Implemented generateCode, findCode, and deleteCode methods.
    - Added VerificationCodeModule as a global module.

---
+++ src/modules/verification/verification.module.ts
@@ -0,0 +1,10 @@
+import { Global, Module } from "@nestjs/common";
+import { VerificationService } from "./verification.service";
+
+@Global()
+@Module({
+    providers: [VerificationService],
+    exports: [VerificationService]
+})
+
+export class VerificationModule {}
